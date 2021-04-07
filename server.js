c34d4d2a2cd90bab9c57cf817e113c0800567a06
Alex Kowalchuk <alexmkowalchuk@gmail.com>
2021-04-07 02:10:32 -0400

    second commit

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.post("/api/user/register",(req,res)=>{
 app.post("/api/user/login",(req,res)=>{
     userService.checkUser(req.body)
     .then((user)=>{
+        console.log("CHECK USER THEN");
         var payload = {
             _id: user._id,
             userName: user.userName
